cacb078b8c10594c2e44292ff65760871082748a
mudit11011 <mudit@achieverspoint.co>
2019-12-17 12:50:54 +0530

    prop type set to required

--- src/components/keypad-container.js
+++ src/components/keypad-container.js
@@ -31,7 +31,6 @@ const KeypadContainer = React.createClass({
         onElementMounted: React.PropTypes.func,
         onPageSizeChange: React.PropTypes.func.isRequired,
         toggleNumAlphabets: React.PropTypes.func.isRequired,
-        numPad: React.PropTypes.bool.isRequired,
         style: React.PropTypes.any,
     },
 
